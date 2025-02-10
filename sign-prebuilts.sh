2d5313208080bd554be4bf406b6dadca7053b760
Koen J <koen@disper.io>
2025-02-10 15:35:30 +0100

    Updated macos prebuilts.

--- sign-prebuilts.sh
+++ sign-prebuilts.sh
@@ -2,6 +2,8 @@
 
 ./sign-dotcefnative.sh "prebuilt/osx-arm64/dotcefnative.app"
 ./sign-dotcefnative.sh "prebuilt/osx-x64/dotcefnative.app"
+rm -rf "prebuilt/osx-arm64/dotcefnative.app.orig"
+rm -rf "prebuilt/osx-x64/dotcefnative.app.orig"
 mv "prebuilt/osx-arm64/dotcefnative.app" "prebuilt/osx-arm64/dotcefnative.app.orig"
 mv "prebuilt/osx-x64/dotcefnative.app" "prebuilt/osx-x64/dotcefnative.app.orig"
 mv "prebuilt/osx-arm64/dotcefnative-signed.app" "prebuilt/osx-arm64/dotcefnative.app"
