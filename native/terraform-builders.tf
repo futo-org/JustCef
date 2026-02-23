2db12ff98d3ed74366ecb4edcf40d44bc2ff7323
Koen J <koen@disper.io>
2026-02-23 20:19:14 +0100

    Reenable security group creation.

--- native/terraform-builders.tf
+++ native/terraform-builders.tf
@@ -29,26 +29,26 @@ variable "cf_r2_bucket" {
   type = string
 }
 
-#resource "aws_security_group" "build_server_justcefnative_sg" {
-#  name        = "build_server_justcefnative_security_group"
-#  description = "Security group for build server allowing SSH access"
-#  vpc_id      = data.aws_vpc.default.id
-#
-#  ingress {
-#    description = "Allow SSH access"
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#}
+resource "aws_security_group" "build_server_justcefnative_sg" {
+  name        = "build_server_justcefnative_security_group"
+  description = "Security group for build server allowing SSH access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # x64 Build Server
 resource "aws_instance" "build_server_justcefnative_x64" {
